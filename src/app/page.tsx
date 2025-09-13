e56ce2c805548d530382b4627968644b20e8da8f
praharshaAdhikari <praharsha101@gmail.com>
2025-09-13 11:58:36 +0545

    added section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,103 +1,60 @@
+"use client";
 import Image from "next/image";
+import { useState } from "react";
 
 export default function Home() {
-  return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+  const [selected, setSelected] = useState(0);
+  const options = ["Social Life", "Personal Life"];
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+  return (
+    <div className="threshold h-dynamic flex justify-end relative">
+      <div className="space-y-8 w-1/2 p-16 overflow-hidden">
+        <h2 className="font-extrabold text-5xl">Welcome</h2>
+        <p>
+          Whether article spirits new her covered hastily sitting her. Money
+          witty books nor son add. Chicken age had evening believe but proceed
+          pretend mrs.
+          <br />
+          <br />
+          At missed advice my it no sister. Miss told ham dull knew see she spot
+          near can. Spirit her entire her called. Detract yet delight written
+          farther his general.
+        </p>
+        <Image src="/signature.png" alt="signature" width={97} height={51} />
+        <button className="ring-2 ring-gray-300 hover:ring-gray-600 px-16 py-4 font-semibold cursor-pointer duration-200">
+          Let's Connect
+        </button>
+        <div className="flex gap-12 py-8">
+          {options.map((option, index) => (
+            <h3
+              key={index}
+              className={`text-4xl cursor-pointer ${
+                selected === index ? "" : "text-gray-300"
+              }`}
+              onClick={() => setSelected(index)}
+            >
+              {option}
+            </h3>
+          ))}
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+      </div>
+
+      <div className="w-1/2 px-32 h-dynamic overflow-y-hidden absolute -top-0 left-0">
+        <Image
+          className="w-full h-full object-cover object-top"
+          src="/nirmal-dhakal.png"
+          alt="nirmal-dhakal"
+          width={530 / 2}
+          height={1166 / 2}
+        />
+      </div>
+
+      <p
+        className="text-gray-100 absolute bottom-0 left-1/2 -translate-x-1/2 font-bold text-nowrap -z-10 w-full"
+        style={{ fontSize: "calc(82rem / 7)" }}
+      >
+        Nirmal Dhakal
+      </p>
     </div>
   );
 }
