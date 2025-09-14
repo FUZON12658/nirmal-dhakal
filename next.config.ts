010442b4dbc90b8ff1b00fb029c0e76c0c450747
fuzon12658 <fuzon12658@gmail.com>
2025-09-14 10:21:37 +0545

    updated content

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  // compiler: {
+  //   removeConsole: {
+  //     exclude: ["error"], // keep console.error, remove others like log, warn, etc.
+  //   },
+  // },
 };
 
 export default nextConfig;
